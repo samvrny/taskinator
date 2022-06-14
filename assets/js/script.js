0c76d9456d0e439aaed13c38d4c5f1d2e3aee92c
samvrny <samvrny@gmail.com>
2022-06-14 17:21:24 -0500

    refactored code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,37 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) { 
     event.preventDefault();
 
     var taskNameInput = document.querySelector("input[name ='task-name']").value;
     var taskTypeInput = document.querySelector("select[name ='task-type']").value;
-    
-    //create list item
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
 
-    //create div to hold task info and add to list item
-    var taskInfoEl = document.createElement("div");
-    //give it a class name
-    taskInfoEl.className = "task-info";
-    //add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class ='task-name'>" + taskNameInput + "</h3><span class = 'task-type'>" + taskTypeInput + "</span>";
-    //make the previous div with the innerHTML a child of the listItemEl (li element)
-    listItemEl.appendChild(taskInfoEl);
+    //package up data an an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
 
-    //add entire list item to list
-    tasksToDoEl.appendChild(listItemEl);
+    //send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+
 };
 
-formEl.addEventListener("submit", createTaskHandler);
+var createTaskEl = function(taskDataObj) {
+      //create list item
+      var listItemEl = document.createElement("li");
+      listItemEl.className = "task-item";
+      //create div to hold task info and add to list item
+      var taskInfoEl = document.createElement("div");
+      //give it a class name
+      taskInfoEl.className = "task-info";
+      //add HTML content to div
+      taskInfoEl.innerHTML = "<h3 class ='task-name'>" + taskDataObj.name + "</h3><span class = 'task-type'>" + taskDataObj.type + "</span>";
+      //make the previous div with the innerHTML a child of the listItemEl (li element)
+      listItemEl.appendChild(taskInfoEl);
+      //add entire list item to list
+      tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", taskFormHandler);
