466f7274987bb03f75c37f8dadbe4aff94cd4372
samvrny <samvrny@gmail.com>
2022-06-15 14:51:34 -0500

    added some buttons!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskIdCounter = 0;
 
 var taskFormHandler = function(event) { 
     event.preventDefault();
@@ -13,7 +14,7 @@ var taskFormHandler = function(event) {
         return false;
     }
     
-    //this is to reset the form every time it gets clicked
+    //this is to reset the form every time it gets clicked THIS WILL BE USED IN THE CHALLENGE TO GENERATE A NEW PAGE EVERY TIME. Things need to be in a form for it to work.
     formEl.reset();
     
     //package up data an an object
@@ -32,6 +33,9 @@ var createTaskEl = function(taskDataObj) {
       var listItemEl = document.createElement("li");
       listItemEl.className = "task-item";
 
+      //add task id (this is from data-*) as a custom attribute THIS IS WHERE data-task-id IS DEFINED
+      listItemEl.setAttribute("data-task-id", taskIdCounter);
+
       //create div to hold task info and add to list item
       var taskInfoEl = document.createElement("div");
       //give it a class name
@@ -41,8 +45,63 @@ var createTaskEl = function(taskDataObj) {
       //make the previous div with the innerHTML a child of the listItemEl (li element)
       listItemEl.appendChild(taskInfoEl);
 
+      //add all the data from the createTaskActions function 
+      var taskActionsEl = createTaskActions(taskIdCounter);
+      listItemEl.appendChild(taskActionsEl);
+
       //add entire list item to list
       tasksToDoEl.appendChild(listItemEl);
+
+      //increase task counter (stored in a global variable at the top, taskIdCounter) for next unique ID, which will be a number starting at 0 and ascending from there.
+      taskIdCounter++;
+
 };
+
+var createTaskActions = function(taskId) {
+    //create div 
+    var actionContainerEl = document.createElement("div");
+    actionContainerEl.className = "task-actions";
+
+    //create edit button
+    var editButtonEl = document.createElement("button");
+    editButtonEl.textContent = "Edit";
+    editButtonEl.className = "btn edit-btn";
+    editButtonEl.setAttribute = ("data-task-id", taskId);
+
+    //make the edit button a child of actionContainerEl div that was just created.
+    actionContainerEl.appendChild(editButtonEl);
+
+    //create delete button
+    var deleteButtonEl = document.createElement("button");
+    deleteButtonEl.textContent = "Delete";
+    deleteButtonEl.className = "btn delete-btn";
+    deleteButtonEl.setAttribute = ("data-task-id", taskId);
+
+    //make the delete button a child of the actionContainerEl div
+    actionContainerEl.appendChild(deleteButtonEl);
+
+    //add a dropdown menu for the tasks. Need to create a <select> element to make a dropdown menu
+    var statusSelectEl = document.createElement("select");
+    statusSelectEl.className = "select-status";
+    statusSelectEl.setAttribute("name", "status-change");
+    statusSelectEl.setAttribute("data-task-id", taskId);
+
+    //make the select menu a child of the actionContainerEl div
+    actionContainerEl.appendChild(statusSelectEl);
+
+    var statusChoices = ["To Do", "In Progress", "Completed"];
+
+    for (var i=0; i < statusChoices.length; i++) {
+        //create option element
+        var statusOptionEl = document.createElement("option");
+        statusOptionEl.textContent = statusChoices[i];
+        statusOptionEl.setAttribute("value", statusChoices[i]);
+
+        //appent these options to the select dropdown menu above (statusSelectEl)
+        statusSelectEl.appendChild(statusOptionEl);
+    }
+
+    return actionContainerEl;
+};//end createTaskActions function
 
 formEl.addEventListener("submit", taskFormHandler);
