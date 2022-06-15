15c2c653c4807190ad6f3e41b4b4f1eeb3030e24
samvrny <samvrny@gmail.com>
2022-06-15 16:37:27 -0500

    added the beginings of being able to edit with the edit button. Set it so the edit button, when selected, the dom knows which is selected.

--- assets/js/script.js
+++ assets/js/script.js
@@ -107,10 +107,22 @@ var createTaskActions = function(taskId) {
 };//end createTaskActions function
 
 var taskButtonHandler = function(event) {
+    //get target element from event
+    var targetEl = event.target;
 
-    if (event.target.matches(".delete-btn")) { // this classes name is actually btn delete-btn. Not sure exactly why this all works but it does.
-        //get the elements task id
-        var taskId = event.target.getAttribute("data-task-id");
+    //edit button was clicked
+    if (targetEl.matches(".edit-btn")) {
+        //get the elements task id...
+        var taskId = targetEl.getAttribute("data-task-id");
+        //...and edit it
+        editTask(taskId);
+    }
+
+    //delete button was clicked
+    if (targetEl.matches(".delete-btn")) { // this classes name is actually btn delete-btn. Not sure exactly why this all works but it does.
+        //get the elements task id...
+        var taskId = targetEl.getAttribute("data-task-id");
+        //...and remove it
         deleteTask(taskId);
     }
 };
@@ -118,6 +130,21 @@ var taskButtonHandler = function(event) {
 var deleteTask = function(taskId) {
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']"); //this line is a demonstration of how/where to use double and single quotes and how insane that can be.
     taskSelected.remove();
+};
+
+var editTask = function(taskId) {
+    //get task list item element
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    //get content form task name and type
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type']").value = taskType;
+    document.querySelector("#save-task").textContent = "Save Task";
+    formEl.setAttribute("data-task-id", taskId);
 };
 
 pageContentEl.addEventListener("click", taskButtonHandler);
